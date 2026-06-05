24c7438d85beafc9ef3eee6842e09c6ba0efd64f
Ahmad. <ahmadjuttboy516@gmail.com>
2026-06-05 11:50:13 +0500

    Push

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i, j = 3;
+    for (i = 1; i <= 5; i++)
+    {
+        printf("\n %d%d", i, j);
+    }
+    return 0;
+}
+
+
+//A funtion is a self-contained piece of code which performs a speciic task
---
+++ while-loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int n = 1;
+    // clrscr();
+    while (n <= 5)
+    {
+        printf("Pakistan\n");
+        n++;
+    }
+    return 0;
+}
